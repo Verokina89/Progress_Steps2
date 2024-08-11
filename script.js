09a10073b89b1effc216807142ed6fba19fa53e1
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-08-11 19:35:50 +0200

    btn ok,but problems with line and css

--- script.js
+++ script.js
@@ -5,21 +5,21 @@ const steps = document.querySelectorAll('.number');
 
 let currentStep = 0; //inicia en el actual.
 
-// //funcion que actualiza los botones (desabilita cuando es necesario)
-// function updateBtns() {
-//     prevBtn.disabled = currentStep === 0; //deshabilita Prev 1er paso.
-//     nextBtn.disabled = currentStep === steps.length - 1; //deshabilita next en el último paso.
-// }
+//funcion que actualiza los botones (desabilita cuando es necesario)
+function updateBtns() {
+    prevBtn.disabled = currentStep === 0; //deshabilita Prev 1er paso.
+    nextBtn.disabled = currentStep === steps.length - 1; //deshabilita next en el último paso.
 
-//funcion que actualiza linea de avance con iluminación.
+}
+
+
+//funcion que actualiza linea.
 function updateMovingLine() {
     const moving = (currentStep / (steps.length -1)) * 100;
-    movingLine = `${moving}%`;
+    movingLine.style.width = `${moving}%`;
+    movingLine.classList.add('active'); //asegura la funcion de la clase 'active' en css
 }
-//para iluminar la línea de avance al actualizar (cuando se genera el movimiento de los pasos).
-movingLine.classList.add('active');
-
-
+// evento para avanzar. 
 nextBtn.addEventListener('click', () => {
     if (currentStep < steps.length - 1) {
         steps[currentStep].classList.remove('active'); //quita la clase "active" del actual.
@@ -38,19 +38,6 @@ prevBtn.addEventListener('click', () => {
     }
     updateBtns(); //actualiza y permite ejecutar mas movimientos sin limites.
 });
-
-// nextBtn.addEventListener('click', () => {
-//     steps[currentStep].classList.remove('active'); //quita la clase 'active' en la posición actual.
-//     currentStep = (currentStep + 1) % steps.length; // avanza al siguiente y vuelve al 0 si esta al final.
-//     steps[currentStep].classList.add('active'); //agrega la clase active al siguiente.
-//     updateMovingLine(); //actualiza la línea de avance.
-// });
-
-// prevBtn.addEventListener('click', () => {
-//     steps[currentStep].classList.remove('active');
-//     currentStep = (currentStep - 1 + steps.length) % steps.length; //retrocede y va al ultimo cuando este en el 1º
-//     updateMovingLine(); //actualiza la línea.
-// })
 
 //actualización de botones y linea de avance.
 updateBtns();
