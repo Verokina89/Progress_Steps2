6cf7e7e3fc4317df140e87de23e56739927eabc3
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-08-12 00:50:57 +0200

    css is missing

--- script.js
+++ script.js
@@ -5,22 +5,16 @@ const steps = document.querySelectorAll('.number');
 
 let currentStep = 0; //inicia en el actual.
 
-//funcion que actualiza los botones (desabilita cuando es necesario)
-// const updateBtns = () => {
-//     prevBtn.disabled = currentStep === 0; //deshabilita Prev 1er paso.
-//     nextBtn.disabled = currentStep === steps.length - 1; //deshabilita next en el último paso.
-// }
-
 const updateMovingLine = () => {
     steps.forEach((step, index) => {
-        index <= currentStep ? step.classList.add('active')
-        : steps.classList.remove('active'); // 
+        index <= currentStep 
+        ? step.classList.add('active')
+        : step.classList.remove('active') 
     })
 
     prevBtn.disabled = currentStep === 0; //deshabilita Prev 1er paso.
     nextBtn.disabled = currentStep === steps.length - 1; //deshabilita next en el último paso.
 }
-
 
 // evento para avanzar. 
 nextBtn.addEventListener('click', () => {
@@ -28,18 +22,17 @@ nextBtn.addEventListener('click', () => {
         currentStep++; //avanza.
         updateMovingLine(); //actualiza la linea.
     }
-    updateBtns(); //actualiza btns.permite realizar mas movimientos sin limites.
+   
 });
 
 prevBtn.addEventListener('click', () => {
     if (currentStep > 0) {
         steps[currentStep].classList.remove('active');
         currentStep--; //retrocede.
-        updateMovingLine
+        updateMovingLine();
     }
-    updateBtns(); //actualiza y permite ejecutar mas movimientos sin limites.
+    // updateBtns(); //actualiza y permite ejecutar mas movimientos sin limites.
 });
 
-//actualización de botones y linea de avance.
-updateBtns();
+
 updateMovingLine();
