6f47f97a7038cdf044b7e91c692a63a0c4d202a3
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-08-11 02:43:16 +0200

    JS in progress; problems with delimiting movements

--- script.js
+++ script.js
@@ -1,5 +1,41 @@
-const btnPrev = document.getElementById('previous');
-const btnNext = document.getElementById('next');
+const prevBtn = document.getElementById('previous');
+const nextBtn = document.getElementById('next');
 const movingLine = document.getElementById('movingLine');
-const number = document.querySelectorAll('.number');
+const steps = document.querySelectorAll('.number');
 
+let currentStep = 0; //inicia en el actual.
+
+//funcion que actualiza los botones (desabilita cuando es necesario)
+function updateBtns() {
+    prevBtn.disabled = currentStep === 0; //deshabilita Prev 1er paso.
+    nextBtn.disabled = currentStep === steps.length - 1; //deshabilita next en el último paso.
+}
+
+//funcion que actualiza linea de avance.
+function updateMovingLine() {
+    const moving = (currentStep / (steps.length -1)) * 100;
+    movingLine = `${moving}%`;
+}
+
+nextBtn.addEventListener('click', () => {
+    if (currentStep < steps.length - 1) {
+        steps[currentStep].classList.remove('active'); //quita la clase "active" del actual.
+        currentStep++;  //avanza.
+        steps[currentStep].classList.add('active'); //agrega la clase "active" al siguiente.
+        updateMovingLine(); //actualiza la linea de avance.
+    }
+    updateBtns(); //actualiza btns.permite realizar mas movimientos sin limites.
+});
+
+prevBtn.addEventListener('click', () => {
+    if (currentStep > 0) {
+        steps[currentStep].classList.remove('active');
+        currentStep--; //retrocede.
+        steps[currentStep].classList.add('active');
+    }
+    updateBtns(); //actualiza y permite ejecutar mas movimientos sin limites.
+});
+
+//actualización de botones y linea de avance.
+updateBtns();
+updateMovingLine();
