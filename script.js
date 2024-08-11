60ed4f61675479814a5973eea4c58d5c10d6bfa0
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-08-11 22:46:29 +0200

    js ok, details missing with css

--- script.js
+++ script.js
@@ -6,25 +6,26 @@ const steps = document.querySelectorAll('.number');
 let currentStep = 0; //inicia en el actual.
 
 //funcion que actualiza los botones (desabilita cuando es necesario)
-function updateBtns() {
+// const updateBtns = () => {
+//     prevBtn.disabled = currentStep === 0; //deshabilita Prev 1er paso.
+//     nextBtn.disabled = currentStep === steps.length - 1; //deshabilita next en el último paso.
+// }
+
+const updateMovingLine = () => {
+    steps.forEach((step, index) => {
+        index <= currentStep ? step.classList.add('active')
+        : steps.classList.remove('active'); // 
+    })
+
     prevBtn.disabled = currentStep === 0; //deshabilita Prev 1er paso.
     nextBtn.disabled = currentStep === steps.length - 1; //deshabilita next en el último paso.
-
 }
 
 
-//funcion que actualiza linea.
-function updateMovingLine() {
-    const moving = (currentStep / (steps.length -1)) * 100;
-    movingLine.style.width = `${moving}%`;
-    movingLine.classList.add('active'); //asegura la funcion de la clase 'active' en css
-}
 // evento para avanzar. 
 nextBtn.addEventListener('click', () => {
     if (currentStep < steps.length - 1) {
-        steps[currentStep].classList.remove('active'); //quita la clase "active" del actual.
-        currentStep++;  //avanza.
-        steps[currentStep].classList.add('active'); //agrega la clase "active" al siguiente.
+        currentStep++; //avanza.
         updateMovingLine(); //actualiza la linea.
     }
     updateBtns(); //actualiza btns.permite realizar mas movimientos sin limites.
@@ -34,7 +35,7 @@ prevBtn.addEventListener('click', () => {
     if (currentStep > 0) {
         steps[currentStep].classList.remove('active');
         currentStep--; //retrocede.
-        steps[currentStep].classList.add('active');
+        updateMovingLine
     }
     updateBtns(); //actualiza y permite ejecutar mas movimientos sin limites.
 });
